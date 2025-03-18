43f787883aad00e7ede155355268fba23711a01e
Roshnn <roshanjabir7@gmail.com>
2025-03-18 17:20:34 +0530

    fix: Fixed Login not working

--- Backend/tasks/serializers.py
+++ Backend/tasks/serializers.py
@@ -19,7 +19,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)  # Hash password
+        user.save()
+        return user
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
